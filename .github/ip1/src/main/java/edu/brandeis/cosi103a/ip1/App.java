ac95a33ca39817d951cc52fc9749053d2d7f7103
Zheng Tang <zhengtang@brandeis.edu>
2025-01-20 03:36:41 +0000

    11

---
+++ .github/ip1/src/main/java/edu/brandeis/cosi103a/ip1/App.java
@@ -0,0 +1,13 @@
+package edu.brandeis.cosi103a.ip1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
